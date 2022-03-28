f4a1bd35213fadcfc045a086548cd2e5f9bb9c60
j-n4m4573 <jg_220@yahoo.com>
2022-03-28 07:13:57 -0700

    [ADD] updates SceneDelegate adds navigation controller, removes storyboards, updates plist to support programatic views

---
+++ NYPL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYPL
+//
+//  Created by Jamar Gibbs on 3/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+    }
+
+
+}
+
---
+++ NYPLTests/NYPLTests.swift
@@ -0,0 +1,22 @@
+//
+//  NYPLTests.swift
+//  NYPLTests
+//
+//  Created by Jamar Gibbs on 3/27/22.
+//
+
+import XCTest
+@testable import NYPL
+
+class NYPLTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+
+}
